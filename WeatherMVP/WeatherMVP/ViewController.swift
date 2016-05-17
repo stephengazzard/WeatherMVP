7bf367f7a642620e0411f95a86b3325522eddafa
stephengazzard <stephen.gazzard@gmail.com>
2016-05-17 15:07:50 +0100

    Create initial project

---
+++ WeatherMVP/WeatherMVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherMVP
+//
+//  Created by Stephen Gazzard on 2016-05-17.
+//  Copyright © 2016 TUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
